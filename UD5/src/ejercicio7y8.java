23009f2e86a5adea264fb83c189061edb6094fd2
Ainara <ainarabliz@gmail.com>
2023-10-03 10:42:10 +0200

    Ejercicios 7 y 8, bucles

---
+++ UD5/src/ejercicio7y8.java
@@ -0,0 +1,18 @@
+
+public class ejercicio7y8 {
+
+	public static void main(String[] args) {
+
+		int num=1;
+		
+		while (num<=100) {
+			System.out.println(num);
+			num++;
+		}
+		System.out.println("Hasta aquí es mediante el bucle While y a continuación mediante el bucle For");
+		
+		for (int digit=1; digit < 101; ++digit) {
+			System.out.println(digit);
+		}
+	}
+}
