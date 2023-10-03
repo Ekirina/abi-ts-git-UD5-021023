b2941ec2efcd87a68df15fde1ddbc9d410b6562d
Ainara <ainarabliz@gmail.com>
2023-10-03 10:56:23 +0200

    Ejercicio 9, divisibles

---
+++ UD5/src/ejercicio9.java
@@ -0,0 +1,14 @@
+
+public class ejercicio9 {
+
+	public static void main(String[] args) {
+		
+		for (int num = 1; num<101; num++){
+			
+			if ((num%2 == 0) && (num%3 == 0)){
+				System.out.println(num + " es divisible por 2 y 3");
+	
+			}
+		}
+	}
+}
