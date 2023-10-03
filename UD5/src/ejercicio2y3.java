c77e0bcc56700b36def2200efaf2d77b586e2a62
Ainara <ainarabliz@gmail.com>
2023-10-03 15:59:38 +0200

    Añadido el mensaje de diferenciación de bloques

--- UD5/src/ejercicio2y3.java
+++ UD5/src/ejercicio2y3.java
@@ -9,6 +9,10 @@ public class ejercicio2y3 {
 	System.out.print("Bienvenido/a "+name);
 	}*/
 	
+	/*Si se quita el tipo comentario del bloque anterior y se añade como comentario el siguiente bloque, 
+	 * funcionará igualmente.
+	 */
+	
 	public static void main(String[] args) {
 		
 		String nombre=JOptionPane.showInputDialog("Introduce tu nombre");
